78baf1e2b2f2f0765b67e3a0764298b501f29448
safym <osafym@gmail.com>
2023-05-19 19:10:54 +0300

    add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  type: 'text' | 'number'
+  name: string
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  required: boolean
+}
+
+const Input = ({type, name, value, onChange, required, ...props}: Props) => {
+  return (
+    <input
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+      required={required}
+    />
+  )
+}
+
+export default Input
