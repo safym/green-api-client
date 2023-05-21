72bc26a0db30094eb4e068befe5dc395f06d5701
safym <osafym@gmail.com>
2023-05-21 19:09:01 +0300

    add chat list cintainer styles

---
+++ src/containers/ChatListContainer/ChatListContainer.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+
+import { ChatItem, MessengerContextType } from '../../@types/messenger'
+
+import { MessengerContext } from '../../context/MessengerProvider'
+
+import ChatPreview from '../../components/ChatPreview/ChatPreview'
+
+import styles from './ChatListContainer.module.scss'
+
+const ChatListContainer: React.FC = () => {
+  // Данные контекста мессенджера
+  const { chatList, setCurrentChat, currentChat } = useContext(MessengerContext) as MessengerContextType
+
+  return (
+    <div className={styles.chatList}>
+      {chatList.map((chatItem, index) => (
+        <ChatPreview
+          key={index}
+          chatItem={chatItem}
+          selected={currentChat.chatId === chatItem.chatId}
+          onClick={() => setCurrentChat(chatItem)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default ChatListContainer
