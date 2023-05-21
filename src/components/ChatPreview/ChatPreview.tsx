de615b9adeab46f9c21a7e811351d31081fd684f
safym <osafym@gmail.com>
2023-05-21 19:06:42 +0300

    add chat preview component style

---
+++ src/components/ChatPreview/ChatPreview.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import cn from 'classnames'
+
+import { ChatItem } from '../../@types/messenger'
+
+import { RiUserFill } from 'react-icons/ri'
+
+import styles from './ChatPreview.module.scss'
+
+interface Props {
+  chatItem: ChatItem
+  selected: boolean
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void
+}
+
+const ChatPreview: React.FC<Props> = ({ chatItem, onClick, selected, ...props }) => {
+  return (
+    <div onClick={onClick} className={cn(styles.chatPreview, { [styles.selected]: selected })}>
+      <div className={styles.wrapper}>
+        <div className={styles.avatar}>
+          <RiUserFill className={styles.icon} />
+        </div>
+        <span className={styles.phoneNumber}>{chatItem.phoneNumber}</span>
+      </div>
+    </div>
+  )
+}
+
+export default ChatPreview
