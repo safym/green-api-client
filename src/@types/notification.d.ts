5f724c705f9b566982018995aa3246d5470cf3d1
safym <osafym@gmail.com>
2023-05-21 03:35:19 +0300

    add notification types

---
+++ src/@types/notification.d.ts
@@ -0,0 +1,65 @@
+export interface Notification {
+  receiptId: number
+  body: {
+    typeWebhook: 'incomingMessageReceived' | 'outgoingAPIMessageReceived' | 'outgoingMessageStatus'
+    instanceData: {
+      idInstance: number
+      wid: string
+      typeInstance: 'whatsapp'
+    }
+    timestamp: number
+    idMessage: string
+    senderData?: {
+      chatId: string
+      chatName: string
+      sender: string
+      senderName: string
+    }
+    messageData?: {
+      typeMessage: 'textMessage'
+      textMessageData: {
+        textMessage: string
+      }
+    }
+    chatId?: string
+    status?: string
+    sendByApi?: boolean
+  }
+}
+
+export interface GroupedNotifications {
+  receiptId: number
+  body: {
+    typeWebhook: 'incomingMessageReceived' | 'outgoingAPIMessageReceived'
+    instanceData: {
+      idInstance: number
+      wid: string
+      typeInstance: 'whatsapp'
+    }
+    timestamp: number
+    idMessage: string
+    senderData: {
+      chatId: string
+      chatName: string
+      sender: string
+      senderName: string
+    }
+    messageData: {
+      typeMessage: 'textMessage'
+      textMessageData: {
+        textMessage: string
+      }
+    }
+    status?: string
+  }
+}
+
+export interface NotificationContextType {
+  notifications: Notification[]
+  setNotifications: Dispatch<SetStateAction<Array<Notification>>>
+  addNotification: (data: Notification) => void
+  groupedNotifications: groupedNotifications[]
+  groupNotifications: () => void
+  notificationsIsLoaded: boolean
+  setNotificationsIsLoaded: Dispatch<SetStateAction<boolean>>
+}
